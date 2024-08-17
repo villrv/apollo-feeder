e7f5fba6b7eec79babfa5632d7dd786a5c849859
villrv <ashley.villar@gmail.com>
2024-08-17 10:42:50 -0400

    update ip

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -2,7 +2,7 @@ import qrcode
 from PIL import Image
 
 # The URL you want to encode in the QR code
-url = "http://127.0.0.1:5000/"
+url = "http://192.168.86.121:8080/"
 
 # Generate the QR code
 qr = qrcode.QRCode(
@@ -25,7 +25,7 @@ poodle = poodle.resize((50, 50))  # Adjust the size as needed
 pos = ((img_qr.size[0] - poodle.size[0]) // 2, (img_qr.size[1] - poodle.size[1]) // 2)
 
 # Paste the poodle image onto the QR code
-img_qr.paste(poodle, pos)
+img_qr.paste(poodle, pos, mask=poodle)
 
 # Save the final QR code image
 img_qr.save("dog_feeder_qr_with_poodle.png")
