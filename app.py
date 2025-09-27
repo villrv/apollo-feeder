6cfa6318a797a6454677c671b9da42fb8397f132
villrv <ashley.villar@gmail.com>
2025-09-27 18:01:16 -0400

    color

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def led_test():
     
     # Turn all LEDs on with a nice Halloween orange color
     for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color(255, 128, 0))  # Orange
+        strip.setPixelColor(i, Color(0, 128, 255))  # Try swapping R and B channels
     strip.show()
     
     # Wait for 3 seconds
